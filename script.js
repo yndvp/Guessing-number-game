43ca1e83110d3f2a27dca18742c0f9b85a7b3850
yndvp <yunna3828@gmail.com>
2021-05-06 22:24:25 +0900

    Update script.js
    remove console.log function

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const displayMessage = function (message) {
 // When press 'check' button
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
